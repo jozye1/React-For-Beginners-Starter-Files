5128a5247da617c8c0e299607b0410feb63059d6
Josue (Jozye) Merisier <ijosueko@gmail.com>
2017-01-03 12:53:47 -0500

    Completed steps from videos 1-21

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,10 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass ({
+  apiKey: "AIzaSyAZu6orHr_pOMqsN_5ECeXDqBLq9M8mexY",
+    authDomain: "catch-of-the-day-jozye.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-jozye.firebaseio.com",
+
+});
+
+export default base;
